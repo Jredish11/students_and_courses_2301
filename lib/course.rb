4c355c41d1357ea566724086801bf16808727d4e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-15 09:34:24 -0700

    feat: Add initialize methods and full methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,20 @@
+class Course
+  attr_accessor :name, :capacity, :students, :full
+
+  def initialize(name, integer)
+    @name = name
+    @capacity = integer
+    @students = []
+    @full = false
+  end
+
+  def full?
+    @full
+  end
+
+  def full
+    @full = true
+  end
+end
+
 
