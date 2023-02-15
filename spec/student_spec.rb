59dbf768a36f1c1ab9d4dc19557efcf7c4783008
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-15 09:22:57 -0700

    test: Add tests for scores, grade average

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe Student do
 
     expect(student.name).to eq("Morgan")
   end
+
+  it 'has an age' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    expect(student.age).to eq(21)
+  end
+
+  it 'has scores' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    expect(student.scores).to eq([])
+  end
+
+  it 'adds scores, checks scores again' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.scores).to eq([89, 78])
+  end
+
+  it 'creates an average score' do
+    student = Student.new({name: "Morgan", age: 21}) 
+
+    student.log_score(89)
+    student.log_score(78)
+
+    expect(student.scores).to eq([89, 78])
+    expect(student.grade).to eq(83.5)
+  end
 end
