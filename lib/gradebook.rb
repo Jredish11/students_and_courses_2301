a6270b72e6d7011b9df295148b7e89ea1627f16d
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-15 10:38:32 -0700

    feat: Add methods for gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,1 +1,19 @@
+class GradeBook
+  attr_reader :instructor, :courses
+  
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
 
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    lists = {}
+    lists = @courses.map do |course|
+      course.students
+    end.flatten
+  end
+end
