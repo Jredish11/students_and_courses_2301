7db6dce20326eb83ac3e7001ca13a4d7c00ee968
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-02-15 09:23:33 -0700

    feat: Add methods for logging score and grade average

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,19 @@
 class Student
-  attr_accessor :name
-  
+  attr_accessor :name, :age, :scores
+
   def initialize(attributes)
     @name = attributes[:name]
-    
+    @age = attributes[:age]
+    @scores = []
+  end
+
+  def log_score(grade_number)
+    @scores << grade_number
+  end
+
+  def grade
+    average = @scores.sum do |score|
+      score.to_f / 2
+      end
   end
 end
